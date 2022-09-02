208b3ed20b4350f533a9441d26eee6be9cb71696
Alvin <alvin.sanchez27@gmail.com>
2022-09-01 22:47:13 -0400

    commit tree 736c0cc2402993b672019c476bbb623f18d637a1

--- enumerables.rb
+++ enumerables.rb
@@ -11,10 +11,11 @@ end
 
 # given an array of spicy foods, **return an array of strings**
 # with the names of each spicy food
-def get_names(spicy_foods)
-  # your code here
-end
 
+def get_names(spicy_foods)
+  spicy_foods.map {|names| "#{names[:name]}"}
+end
+binding.pry
 # given an array of spicy foods, **return an array of hashes** 
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
